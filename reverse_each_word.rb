7c157eec3357118ec497b75c0b5a6fce815be69a
David Foote <david.a.foote@gmail.com>
2019-06-06 22:10:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,4 +9,7 @@ end
 
 
 def reverse_each_word(sentence)
-  
+  original_sentence = sentence.split(" ")
+  reverse_array = []
+  original_sentence.collect do |sentence|
+  reverse_array << sentence.reverse
