1acbd6cdbb396d7b750026a580075dfa5f6f55e4
David Foote <david.a.foote@gmail.com>
2019-06-06 22:04:45 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  original_sentence = sentence.split(/, /)
+  reverse_array = []
+  original_sentence.each do |sentence|
+  reverse_array << sentence.reverse!
+  end
+  reverse_array.join(" ")
+end
+    
